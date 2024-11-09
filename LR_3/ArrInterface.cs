7e1d3969bc3c1dc3a53cce71348b57df940ee1ac
Cloky2205 <motek.efim@gmail.com>
2024-11-09 13:13:56 +0500

    Add files via upload

---
+++ LR_3/ArrInterface.cs
@@ -0,0 +1,11 @@
+﻿public interface ArrInterface
+{
+    bool find(long searchValue);
+    long findmax();
+    long findmin();
+    bool insert(long value);
+    bool delete(long value);
+    void display();
+    int getSize();
+}
+
