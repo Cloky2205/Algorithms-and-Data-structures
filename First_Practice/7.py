2f55fa338da63b54baf508f4baadf3a7ee6ab496
Cloky2205 <motek.efim@gmail.com>
2024-11-09 10:39:56 +0500

    Delete First_Practice directory

--- First_Practice/7.py
+++
@@ -1,7 +0,0 @@
-#https://acmp.ru/index.asp?main=task&id_task=839
-
-a = str(input())
-if '0' in a:
-    print('NO')
-else:
-    print('YES')
--- First_Practice/8.py
+++
@@ -1,5 +0,0 @@
-#https://acmp.ru/index.asp?main=task&id_task=81
-
-a = int(input())
-arr = [int(i) for i in input().split()]
-print(min(arr),max(arr))
