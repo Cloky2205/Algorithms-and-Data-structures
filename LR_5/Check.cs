93a0186781643593ec933c679aeb52d0d7e8b604
Cloky2205 <motek.efim@gmail.com>
2024-11-19 15:27:19 +0500

    Add files via upload

---
+++ LR_5/Check.cs
@@ -0,0 +1,36 @@
+﻿using LR4;
+
+Random random = new Random();
+int size = 10000;
+HighInterfaceArrayImpl array1 = new HighInterfaceArrayImpl(size);
+
+for (int i = 0; i < size; i++)
+{ 
+    array1.insert(random.NextInt64(100000));
+}
+
+array1.bubbleSort();
+
+
+
+HighInterfaceArrayImpl array2 = new HighInterfaceArrayImpl(size);
+
+for (int i = 0; i < size; i++)
+{
+    array2.insert(random.NextInt64(100000));
+}
+array2.insertionSort();
+
+
+
+
+
+HighInterfaceArrayImpl array3 = new HighInterfaceArrayImpl(size);
+
+for (int i = 0; i < size; i++)
+{
+    array3.insert(random.NextInt64(100000));
+}
+array3.selectionSort();
+
+Console.ReadLine(); 
