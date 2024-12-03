bdc487c776ace3054fd6d7fc35b0e7d9c38839e7
Cloky2205 <motek.efim@gmail.com>
2024-12-03 21:57:42 +0500

    Add files via upload

---
+++ LR_6/Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using Stack;
+
+bool polindrome = true;
+string num = Console.ReadLine();
+int maxSize = num.Length;
+StackImpl theStack = new StackImpl(maxSize);
+for (int i = 0; i < maxSize; i++)
+{
+    theStack.push(num[i]);
+}
+    
+if (maxSize % 2 == 0)
+{
+    StackImpl newStack = new StackImpl(maxSize / 2);
+    while (!newStack.isFull())
+    {
+        newStack.push(theStack.pop());
+    }
+    for (int i = 0; i < maxSize / 2; i++)
+    {
+        if (newStack.pop() == theStack.pop()) ;
+        else { polindrome = false; }
+    }
+
+}
+else
+{
+    
+    StackImpl newStack = new StackImpl((maxSize - 1) / 2);
+    while (!newStack.isFull())
+    {
+        newStack.push(theStack.pop());
+    }
+    theStack.pop();
+    for (int i = 0; i < (maxSize - 1) / 2; i++)
+    {
+     
+        if (newStack.pop() == theStack.pop()) ;
+        else { polindrome = false; }
+    }
+}
+
+
+    if (polindrome) { Console.WriteLine("Строка является полиндромом"); }
+else { Console.WriteLine("Строка не является полиндромом"); }
