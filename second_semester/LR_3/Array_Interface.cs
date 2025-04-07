9d8b0c5fd48b23c92811627b4d0cb3e5e0c81637
Cloky2205 <motek.efim@gmail.com>
2025-04-07 22:49:25 +0500

    Add files via upload

---
+++ second_semester/LR_3/Array_Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_3
+{
+    public interface ClassArrayInterface
+    {
+        bool find(long searchValue);
+        void swap(int index1, int index2);
+        int partition(int leftIndex, int rightIndex, long pivot);
+        void qsort();
+        void msort();
+        void shsort();
+        void quickSort(int left, int right);
+        void insertionSort(int leftIndex, int rightIndex);
+        long medianOfThreePoints(int leftIndex, int rightIndex);
+        bool contains(long searchValue);
+        long findmax();
+        long findmin();
+        void insert(long value);
+        bool delete(long value);
+        void display();
+        int getSize();
+    }
+}
